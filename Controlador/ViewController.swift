36c4915809d04d828c13e31d5db93676a7717607
Fabian Chacon <fnchacon@gmail.com>
2017-11-08 19:22:36 -0800

    Commit file 1

---
+++ Controlador/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Controlador
+//
+//  Created by Fabian Chacon on 08/11/17.
+//  Copyright © 2017 Fabian Chacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    print("esta es una prueba de texto")
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
